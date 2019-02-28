f2c11aa65d3bd6da3af2f6a28158d45501932e2e
Leif Brooks <leifbrooks@yahoo.com>
2019-02-28 10:39:28 -0500

    refactor: Move A Star tests to appropriate class

--- src/test/java/A_StarTest.java
+++ src/test/java/A_StarTest.java
@@ -4,6 +4,8 @@ import models.Point;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.PriorityQueue;
+
 public class A_StarTest {
 
     @Test
@@ -12,6 +14,113 @@ public class A_StarTest {
         A_Star a_star = new A_Star();
         a_star.setHeuristicMultiplier(1);
         Assert.assertEquals(5.65, a_star.calculateHCost(1, 1, n), .01);
+    }
+
+    @Test
+    /**
+     * Use fCost to compare
+     */
+    public void compareToFCost() {
+        Node n1 = new Node(new Point(0, 0), 10, false);
+        n1.setfCost(10);
+        Node n2 = new Node(new Point(1, 0), 10, false);
+
+
+        //less than
+        n2.setfCost(11);
+        Assert.assertEquals(n1.compareTo(n2), -1);
+
+        //equal
+        n2.setfCost(9);
+        Assert.assertEquals(n1.compareTo(n2), 1);
+    }
+
+    @Test
+    /**
+     * If fCost is Tied, use hCost
+     */
+    public void compareToFCostTie() {
+        Node n1 = new Node(new Point(0, 0), 10, false);
+        n1.setfCost(10);
+        n1.sethCost(5);
+        Node n2 = new Node(new Point(1, 0), 10, false);
+        n2.setfCost(10);
+
+        //less than
+        n2.sethCost(6);
+        Assert.assertEquals(n1.compareTo(n2), -1);
+
+        //equal
+        n2.sethCost(5);
+        Assert.assertEquals(n1.compareTo(n2), 0);
+
+        //greater than
+        n2.sethCost(4);
+        Assert.assertEquals(n1.compareTo(n2), 1);
+    }
+
+    @Test
+    /**
+     * PriorityQueue sorts from minimum fCost/hCost to maximum
+     */
+    public void priorityQueueCostsNotEqual() {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        Node n1 = new Node(new Point(0, 0), 10, false);
+        Node n2 = new Node(new Point(1, 0), 10, false);
+
+        n1.setfCost(5);
+        n2.setfCost(10);
+
+        pq.add(n1);
+        pq.add(n2);
+        Assert.assertEquals(n1, pq.poll());
+
+        n1.setfCost(5);
+        n2.setfCost(4);
+        pq.clear();
+        pq.add(n1);
+        pq.add(n2);
+        Assert.assertEquals(n2, pq.poll());
+
+        n1.setfCost(4);
+        n2.setfCost(4);
+        n1.sethCost(4);
+        n2.sethCost(10);
+        pq.clear();
+        pq.add(n1);
+        pq.add(n2);
+
+        Assert.assertEquals(n1, pq.poll());
+
+        n1.setfCost(4);
+        n2.setfCost(4);
+        n1.sethCost(10);
+        n2.sethCost(4);
+        pq.clear();
+        pq.add(n1);
+        pq.add(n2);
+
+        Assert.assertEquals(n2, pq.poll());
+
+    }
+
+    @Test
+    public void priorityQueueCostsEqual() {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        Node n1 = new Node(new Point(0, 0), 10, false);
+        n1.setfCost(5);
+        n1.sethCost(5);
+        Node n2 = new Node(new Point(1, 0), 10, false);
+        n2.setfCost(5);
+        n2.sethCost(5);
+        Node n3 = new Node(new Point(1, 1), 10, false);
+        n3.setfCost(10);
+        n3.sethCost(10);
+
+        pq.add(n1);
+        pq.add(n2);
+        pq.add(n3);
+        Assert.assertEquals(5, pq.poll().getfCost(), .001);
     }
 
 }
